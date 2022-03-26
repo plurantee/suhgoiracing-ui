6834f7ef0abd73cd9bbf745a7a59ad4c5f25df5a
Florante Rapio <rapioflorante1@gmail.com>
2022-03-26 16:40:42 +0800

    asd

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v13.0&appId=539731097461396&autoLogAppEvents=1" nonce="v2l9fMis"></script>
+        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v13.0&appId=539731097461396&autoLogAppEvents=1" nonce="v2l9fMis"></script>
         <AppHeader />
         <Nuxt />
         <AppFooter />
